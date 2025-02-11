f3acd24f2b109e0caf854e5140409e382b5a9c30
ahlaan <mohamedahlan58@gmail.com>
2025-02-11 17:49:00 +0530

    finished

--- src/FlashcardList.js
+++ src/FlashcardList.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Flashcard from './Flashcard'
+import Flashcard from './Flashcard';
 
-export default function FlashcardList({Flashcards}) {
+export default function FlashcardList({ flashcards }) {
   return (
-    <div className='card-grid'>
-        {Flashcards.map(flashcard => {
-            return <Flashcard flashcard={flashcard} key={flashcard.id} />
-        })}
+    <div className="card-grid">
+      {flashcards.map(flashcard => {
+        return <Flashcard flashcard={flashcard} key={flashcard.id} />
+      })}
     </div>
   )
 }
